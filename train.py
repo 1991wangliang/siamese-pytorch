257ea121306261508f152329fd52bb5d5b440e33
lorne <1991wangliang@gmail.com>
2022-07-05 17:32:30 +0800

    Update train.py

--- train.py
+++ train.py
@@ -38,8 +38,13 @@ if __name__ == '__main__':
     Flags(sys.argv)
 
     data_transforms = transforms.Compose([
-        transforms.RandomAffine(15),
-        transforms.ToTensor()
+#         transforms.RandomAffine(15),
+#         transforms.ToTensor()
+        
+        transforms.Resize(320),
+        transforms.CenterCrop(320),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
 
