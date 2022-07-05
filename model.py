567300d47756b6b3db8a2f0efb0b904391409a76
lorne <1991wangliang@gmail.com>
2022-07-05 17:31:25 +0800

    Update model.py

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ class Siamese(nn.Module):
     def __init__(self):
         super(Siamese, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(1, 64, 10),  # 64@96*96
+            nn.Conv2d(3, 64, 10),  # 64@96*96
             nn.ReLU(inplace=True),
             nn.MaxPool2d(2),  # 64@48*48
             nn.Conv2d(64, 128, 7),
@@ -20,8 +20,9 @@ class Siamese(nn.Module):
             nn.Conv2d(128, 256, 4),
             nn.ReLU(),   # 256@6*6
         )
-        self.liner = nn.Sequential(nn.Linear(9216, 4096), nn.Sigmoid())
+        self.liner = nn.Sequential(nn.Linear(262144, 4096), nn.Sigmoid())
         self.out = nn.Linear(4096, 1)
+        self.sigmoid = torch.nn.Sigmoid()
 
     def forward_one(self, x):
         x = self.conv(x)
@@ -34,8 +35,8 @@ class Siamese(nn.Module):
         out2 = self.forward_one(x2)
         dis = torch.abs(out1 - out2)
         out = self.out(dis)
-        #  return self.sigmoid(out)
-        return out
+        return self.sigmoid(out)
+#         return out
 
 
 # for test
